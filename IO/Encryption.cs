e7f7ac6753b04ebff730f24628adfb2209a75f9e
solhope23 <s526413103@gmail.com>
2025-05-26 11:39:29 +0300

    סיימתי קלאס הצפנה

--- IO/Encryption.cs
+++ IO/Encryption.cs
@@ -1,1 +1,35 @@
 ﻿
+public static class Encryption
+{
+    public static char AtbashChar(char Letter)
+    {
+        string[] AllLetters = { "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh", "Ii", "Jj", "Kk", "Ll", "Mm",
+                                "Nn", "Oo", "Pp", "Qq", "Rr", "Ss", "Tt", "Uu", "Vv", "Ww", "Xx", "Yy", "Zz" };
+
+        for (int i = 0; i < AllLetters.Length; i++ )
+        {
+            if (Letter == AllLetters[i][0])
+            {
+                Letter = AllLetters[AllLetters.Length - 1 - i][0];
+                break;
+            }
+            else if (Letter == AllLetters[i][1])
+            {
+                Letter = AllLetters[AllLetters.Length - 1 - i][1];
+                break;
+            }
+        }
+        return Letter;
+    }
+
+    public static string AtbashString(string String)
+    {
+        string NewString = "";
+
+        foreach (char Letter in String)
+        {
+            NewString += Encryption.AtbashChar(Letter);
+        }
+        return NewString;
+    }
+}
--- IO/Program.cs
+++ IO/Program.cs
@@ -8,7 +8,7 @@ namespace IO
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
         }
     }
